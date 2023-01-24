5ce498587255e47064e27e7678a8527ead6d17d3
KDW03 <aszx0628@naver.com>
2023-01-24 20:03:08 +0900

    [BUD-15] feat : 로그인 여부에 따른 네비게이션 로직 구현(#5)

--- app/src/main/java/com/nocapstone/buddyvet/MainActivity.kt
+++ app/src/main/java/com/nocapstone/buddyvet/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.View
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.AppBarConfiguration
@@ -17,20 +18,39 @@ class MainActivity : AppCompatActivity(), MainActivityUtil {
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-
         initAppBar()
         initBottomNav()
+
+        setStartDestination(isLogin())
+    }
+
+    /**
+     * login 여부에 따른 startDestination 조작을 위해
+     * main graph를 가져와서 startDestination을 조작 후 변경내용을 적용한다.
+     * */
+
+    private fun setStartDestination(isLogin: Boolean) {
+        val navGraph = navController.navInflater.inflate(R.navigation.nav_main)
+        if (isLogin) navGraph.setStartDestination(R.id.home)
+        else navGraph.setStartDestination(com.nocapstone.onboarding.R.id.nav_onboarding)
+        navController.setGraph(navGraph, null)
+    }
+
+    // todo 로그인 체크 로직
+    private fun isLogin(): Boolean {
+        return false
     }
 
     private fun initBottomNav() {
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.fragment_containerView) as NavHostFragment
-        val navController = navHostFragment.navController
+        navController = navHostFragment.navController
 
         appBarConfiguration = AppBarConfiguration(
             setOf(R.id.home, R.id.diary, R.id.community, R.id.setting)
@@ -58,9 +78,12 @@ class MainActivity : AppCompatActivity(), MainActivityUtil {
         binding.topAppBar.visibility = View.VISIBLE
     }
 
+    /** [MainActivityUtil] */
+
     override fun setToolbarTitle(newTitle: String) {
         binding.toolbar.title = newTitle
     }
+
 
     override fun navigateToHome(start: Fragment) {
         start.findNavController().navigate(R.id.home)
