66dcad42b6ee4960dfdde9fb4a9361e5c7299abe
KDW03 <aszx0628@naver.com>
2023-01-24 14:18:53 +0900

    feat : App-navigation 코드 구현 (#1)

--- app/src/main/java/com/nocapstone/buddyvet/MainActivity.kt
+++ app/src/main/java/com/nocapstone/buddyvet/MainActivity.kt
@@ -2,10 +2,47 @@ package com.nocapstone.buddyvet
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
+import com.nocapstone.buddyvet.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        initAppBar()
+        initBottomNav()
+    }
+
+    private fun initBottomNav() {
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragment_containerView) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        appBarConfiguration = AppBarConfiguration(
+            setOf(R.id.home, R.id.diary, R.id.community, R.id.setting)
+        )
+
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration)
+        binding.bottomNavigationView.run {
+            setupWithNavController(navController)
+            itemIconTintList = null
+        }
+    }
+
+    private fun initAppBar() {
+        setSupportActionBar(binding.toolbar)
+        supportActionBar?.run {
+            setDisplayShowTitleEnabled(false)
+            setDisplayHomeAsUpEnabled(true)
+        }
     }
 }
