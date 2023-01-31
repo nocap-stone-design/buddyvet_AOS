159a0bc8f02adf9f67b60fc0d8b4a7d34b3ad806
kimdowoo <asasa0628@gs.cwnu.ac.kr>
2023-01-31 16:15:46 +0900

    [BUD-21] feat : onBoarding 알림 권한 13+ 대응 구현(#7)

--- onboarding/src/main/java/com/nocapstone/onboarding/OnBoardingViewPagerFragment.kt
+++ onboarding/src/main/java/com/nocapstone/onboarding/OnBoardingViewPagerFragment.kt
@@ -2,24 +2,29 @@ package com.nocapstone.onboarding
 
 import android.Manifest
 import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.navigation.fragment.findNavController
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.google.android.material.snackbar.Snackbar
 import com.nocapstone.common_ui.DialogForPermission
 import com.nocapstone.common_ui.MainActivityUtil
 import com.nocapstone.onboarding.databinding.FragmentOnBoardingViewPagerBinding
+import kotlin.math.log
 
-private const val NUM_PAGES = 3
 
 class OnBoardingViewPagerFragment : Fragment() {
 
+
     private var _binding: FragmentOnBoardingViewPagerBinding? = null
-    val binding get() = _binding!!
+    private val binding get() = _binding!!
+    var viewpagerNum: Int = 0
 
     private val messageArray = arrayOf("gps 권한", "알림 권한", "카메라 권한")
     private val imageArray = arrayOf(R.drawable.img_gps, R.drawable.img_gps, R.drawable.img_gps)
@@ -29,7 +34,6 @@ class OnBoardingViewPagerFragment : Fragment() {
             Manifest.permission.ACCESS_FINE_LOCATION,
         ),
         arrayOf(Manifest.permission.CAMERA),
-        //todo 13+에 대응
         arrayOf(Manifest.permission.POST_NOTIFICATIONS)
     )
 
@@ -37,17 +41,18 @@ class OnBoardingViewPagerFragment : Fragment() {
         ActivityResultContracts.RequestMultiplePermissions()
     ) { permissions ->
         if (permissions.all { it.value }) {
-            if (binding.viewpager2.currentItem == NUM_PAGES-1) {
+            if (binding.viewpager2.currentItem == viewpagerNum - 1) {
+               /*
                 (activity as MainActivityUtil).run {
                     navigateToHome(this@OnBoardingViewPagerFragment)
-                }
+                }*/
+                findNavController().navigate(R.id.next)
             }
             binding.viewpager2.currentItem++
         } else {
             Snackbar.make(binding.root, "앱을 사용하기 위해 권한 허용을 해주세요!", Snackbar.LENGTH_SHORT).show()
         }
     }
-
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -60,6 +65,7 @@ class OnBoardingViewPagerFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
 
         binding.viewpager2.let {
             it.adapter = OnBoardingViewPagerAdapter(this)
@@ -99,7 +105,8 @@ class OnBoardingViewPagerFragment : Fragment() {
 
     private inner class OnBoardingViewPagerAdapter(fragment: Fragment) :
         FragmentStateAdapter(fragment) {
-        override fun getItemCount(): Int = NUM_PAGES
+
+        override fun getItemCount(): Int = setViewpagerNum()
 
         override fun createFragment(position: Int): Fragment {
             return when (position) {
@@ -109,5 +116,9 @@ class OnBoardingViewPagerFragment : Fragment() {
             }
         }
 
+        fun setViewpagerNum(): Int{
+            viewpagerNum = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) 3 else 2
+            return viewpagerNum
+        }
     }
 }
